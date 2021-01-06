510c5824f7ad9a8751404a1daca310c208a11747
Calvin Wu <calvinwu4@gmail.com>
2021-01-06 05:41:43 -0500

    Added back project achievements

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -28,7 +28,7 @@ const Projects = () => {
         <div className="project-wrapper">
           <Title title="Projects" />
           {projects.map((project) => {
-            const { title, info, info2, url, repo, img, id } = project;
+            const { title, info, info2, url, repo, img, id, achievement } = project;
 
             return (
               <Row key={id}>
@@ -48,6 +48,14 @@ const Projects = () => {
                             'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
                         </p>
                         <p className="mb-4">{info2 || ''}</p>
+                        <div
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          id="project-achievement"
+                          className="text-color-main"
+                        >
+                          {achievement}
+                        </div>
                       </div>
                       <a
                         target="_blank"
