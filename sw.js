c79ddf93f136c4595e3cd504c34e0ee952f1e884
Calvin Wu <calvinwu4@gmail.com>
2023-09-22 19:37:50 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f99716b8ffa61d3776d9.js"
+    "url": "webpack-runtime-626c331119a924336c05.js"
   },
   {
     "url": "framework-643cd9d57cfac08fd2c4.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1908e04768c98461d62b920c61f27f4a"
+    "revision": "691def16701740c1564d0e13cfbb0491"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-73eb8154f41d356e5f76.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "97fc01dea7c5fd0337295e628805cd6c"
+    "revision": "f9470fd107639f7a4085d349014c9980"
   },
   {
     "url": "polyfill-52b26d7d9106be8a9a55.js"
