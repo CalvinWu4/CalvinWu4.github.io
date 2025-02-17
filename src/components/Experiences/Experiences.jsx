f0fc152d4b010e9eababfc0828ea8e0ec5417f34
Calvin Wu <26044296+CalvinWu4@users.noreply.github.com>
2025-02-17 18:11:03 +0000

    update dependencies

--- src/components/Experiences/Experiences.jsx
+++ src/components/Experiences/Experiences.jsx
@@ -1,5 +1,6 @@
-import React, { useContext} from 'react';
-import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
+import React, { useContext } from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import { Container } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
 import Title from '../Title/Title';
@@ -8,46 +9,39 @@ const Experiences = () => {
   const { experiences } = useContext(PortfolioContext);
 
   return (
-    
     <section id="experiences">
-      
       <Container>
-        <div className="experience-wrapperr">
+        <div className="experience-wrapper">
           <Title title="Experiences" />
-          <Timeline lineColor={'#ddd'}>
+          <VerticalTimeline lineColor="#000000">
             {experiences.map((experience) => {
               const { title, organization, location, url, description, img, date, color, id } = experience;
               return (
-                <TimelineItem
+                <VerticalTimelineElement
                   key={id}
-                  dateText={date}
-                  style={{ color: color }}
-                  dateInnerStyle={{ background: color }}
+                  className="vertical-timeline-element"
+                  // date={date}
+                  iconStyle={{ background: color, color: '#fff' }}
                 >
-                  <div class="card">
-                    <div class="img-hover-zoom-large">
-                      <img src={img} class="image-large" alt={organization}/>
+                  <div className="card">
+                    <div className="img-hover-zoom-large">
+                      <img src={img} className="image-large" alt={organization} />
                     </div>
-                    <div class="organization-container">
-                      <a class="nav-item" href={url} target="_blank">{organization}</a>
+                    <div className="organization-container">
+                      <a className="nav-item" href={url} target="_blank" rel="noopener noreferrer">
+                        {organization}
+                      </a>
                     </div>
-                    
-                    <br/>
-
-                    <h3>{title}</h3>
-                    <h4>{location}</h4>
-                    <br/>
-
-                    <p>
-                      {description}
-                    </p>
+                    <br />
+                    <h3 className="vertical-timeline-element-title">{title}</h3>
+                    <h4 className="vertical-timeline-element-subtitle">{location}</h4>
+                    <br />
+                    <p>{description}</p>
                   </div>
-                </TimelineItem>
+                </VerticalTimelineElement>
               );
             })}
-
-          </Timeline>
-          
+          </VerticalTimeline>
         </div>
       </Container>
     </section>
